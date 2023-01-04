64d411736434ad0341d249f794f209429e44666e
masaharu <dabao456democebu3@gmail.com>
2023-01-04 13:43:31 +0900

    課題7

---
+++ MasaKadai7/HoneydewViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HoneydewViewController.swift
+//  MasaKadai7
+//
+//  Created by Mina on 2023/01/02.
+//
+
+import UIKit
+
+class HoneydewViewController: UIViewController {
+    @IBOutlet private weak var numberInput1TextField: UITextField!
+    @IBOutlet private weak var numberInput2TextField: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
+
+    @IBAction private func resultButton(_ sender: Any) {
+        let input1 = Int(numberInput1TextField.text ?? "") ?? 0
+        let input2 = Int(numberInput2TextField.text ?? "") ?? 0
+        let total = input1 - input2
+
+        resultLabel.text = String(total)
+        self.view.endEditing(true)
+    }
+
+    // 画面をタップしたらソフトウェアキーボードを隠す処理
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
